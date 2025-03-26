6501c5c9cb1a56fd5b50aff690790adcdf21e809
jlangon <jlangon@PC111906.dptoinformatica.iesfuengirola1.net>
2025-03-26 19:38:51 +0100

    First Commit

--- src/bingo/GeneradorCartones.java
+++ src/bingo/GeneradorCartones.java
@@ -1,5 +1,10 @@
 package bingo;
 
+/**
+ * Clase para generar cartones del bingo
+ * @author Javier Lanzas Gonzalez
+ * @author Samuel Donato Muñoz Povedano
+ */
 public class GeneradorCartones {
     
 }
